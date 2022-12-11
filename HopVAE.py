Tried to fix backprop problem

HopVAE.py
@@ -221,7 +221,7 @@ class HopVAE(nn.Module):
 
         if self.fit_prior:
             #start by assuming that num_categories and num_levels are the same 
-            z_pred = self.prior(z_rounded)
+            z_pred = self.prior(z_rounded.type(torch.int64))
 
             z_cross_entropy = F.cross_entropy(z_pred, z_rounded.long().detach(), reduction='none')
             z_prediction_error = z_cross_entropy.mean(dim=[1,2,3]) * np.log2(np.exp(1))

main.py
@@ -162,7 +162,7 @@ def main():
     output_location = f'outputs/{config.data_set}-{config.image_size}.ckpt'
 
     model = HopVAE(hop_config, prior_config, device).to(device)
-    if os.path.exists(checkpoint_location):
+    '''if os.path.exists(checkpoint_location):
         #model.load_state_dict(torch.load(checkpoint_location, map_location=device))
         pre_state_dict = torch.load(checkpoint_location, map_location=device)
         to_delete = []
@@ -176,7 +176,8 @@ def main():
         for key in model.state_dict().keys():
             if key not in pre_state_dict.keys():
                 pre_state_dict[key] = model.state_dict()[key]
-        model.load_state_dict(pre_state_dict)
+        model.load_state_dict(pre_state_dict)'''
+    model.load_state_dict(torch.load(checkpoint_location, map_location=device))
 
     optimiser = optim.Adam(model.parameters(), lr=config.learning_rate, amsgrad=False)
     scheduler = optim.lr_scheduler.ExponentialLR(optimiser, gamma=config.gamma)