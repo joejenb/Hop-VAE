6d9ffb1faeb6f5365be6c865b82dc042ea1c817e
Joe Jenner-Bailey <joe_jenner_bailey@Joes-Mac-mini.home>
2022-12-12 12:44:54 +0000

    Seemed to work so now allowing for update of the encoder

--- HopVAE.py
+++ HopVAE.py
@@ -221,7 +221,7 @@ class HopVAE(nn.Module):
         z_rounded_diff = z_rounded - z
         z_rounded = (z + z_rounded_diff) / self.num_levels
 
-        z_quantised = self.hopfield(z_rounded.detach())
+        z_quantised = self.hopfield(z_rounded)
         #z_quantised = self.hopfield(z)
         z_quantised = z_quantised.view(-1, self.representation_dim, self.representation_dim, self.embedding_dim)
         z_quantised = z_quantised.permute(0, 3, 1, 2).contiguous()
