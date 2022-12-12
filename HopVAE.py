e0db394182afb1baa54ff440908829c5c7b7caab
Joe Jenner-Bailey <joe_jenner_bailey@Joes-Mac-mini.home>
2022-12-12 13:06:50 +0000

    Tried using clamp for preservation of gradient

--- HopVAE.py
+++ HopVAE.py
@@ -218,8 +218,9 @@ class HopVAE(nn.Module):
 
         z = z * self.num_levels
         z_rounded = torch.round(z)
-        z_rounded_diff = z_rounded - z
-        z_rounded = (z + z_rounded_diff) / self.num_levels
+        #z_rounded_diff = z_rounded - z
+        #z_rounded = (z + z_rounded_diff) / self.num_levels
+        z_rounded = torch.clamp(z, min=z_rounded, max=z_rounded)
 
         z_quantised = self.hopfield(z_rounded)
         #z_quantised = self.hopfield(z)
