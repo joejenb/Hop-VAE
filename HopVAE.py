64b3ecdef3fb4cc6a52484e173daac5d83b90513
Joe Jenner-Bailey <joe_jenner_bailey@Joes-Mac-mini.home>
2022-12-14 11:38:12 +0000

    Switched to usign sigmoid

--- HopVAE.py
+++ HopVAE.py
@@ -197,7 +197,7 @@ class HopVAE(nn.Module):
 
             #z_indices = F.relu(self.post_vq_conv(z_indices))
             #z_indices = 1 - F.relu(1 - z_indices)
-            z_indices = F.tanh(self.post_vq_conv(z_indices))
+            z_indices = F.sigmoid(self.post_vq_conv(z_indices))
             z_indices_quantised = straight_through_round(z_indices * (self.num_levels - 1))
 
             z_indices_quantised = z_indices_quantised.view(-1, self.representation_dim, self.representation_dim, self.index_dim)
@@ -235,7 +235,7 @@ class HopVAE(nn.Module):
 
         #z_indices = F.relu(self.post_vq_conv(z_indices))
         #z_indices = 1 - F.relu(1 - z_indices)
-        z_indices = F.tanh(self.post_vq_conv(z_indices))
+        z_indices = F.sigmoid(self.post_vq_conv(z_indices))
         z_indices_quantised = straight_through_round(z_indices * (self.num_levels - 1))
         z_indices = z_indices_quantised / (self.num_levels - 1)
 
