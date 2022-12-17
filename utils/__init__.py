83cc23406686cc07c8e3075d8544382a4d4d6444
Joe Jenner-Bailey <joe_jenner_bailey@Joes-Mac-mini.home>
2022-12-17 18:12:56 +0000

    Fixed problem with config

--- utils/__init__.py
+++ utils/__init__.py
@@ -26,6 +26,7 @@ def get_prior_optimiser(config, prior):
     if config.prior == "PixelCNN":
         from priors.PixelCNN.configs.mnist_8_config import config as prior_config
 
+    prior_config = MakeConfig(prior_config)
     optimiser = optim.Adam(prior.parameters(), lr=prior_config.learning_rate)
     scheduler = optim.lr_scheduler.ExponentialLR(optimiser, gamma=prior_config.gamma)
 
