4d9d60cef5522e96cc0432247ae639e15df7db76
Joe <joejbailey@icloud.com>
2023-01-04 00:38:02 +0000

    Reduced num hiddens

--- priors/VAE/configs/mnist_8_config.py
+++ priors/VAE/configs/mnist_8_config.py
@@ -11,7 +11,7 @@ config["momentum"] = 0.1
 config["gamma"] = 0.95
 config["weight_decay"] = 0.0
 
-config["num_hiddens"] = 4096
+config["num_hiddens"] = 128
 config["num_residual_layers"] = 2
 config["num_residual_hiddens"] = 32
 config["num_filters"] = 64
