2ff101600fc817b616539f8a9e6c97222a008dae
Joe Jenner-Bailey <joe@ilasp-mbp02.local>
2023-02-23 18:14:06 +0000

    Added in config for training on celeba dataset

--- utils/__init__.py
+++ utils/__init__.py
@@ -83,6 +83,12 @@ def get_prior(config, device):
 
 
 def get_data_loaders(config, PATH):
+    transform=transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Resize(config.image_size),
+            transforms.Normalize((0.5,0.5,0.5), (1.0,1.0,1.0))
+        ])
+
     if config.data_set == "MNIST":
         transform = transforms.Compose([
                 transforms.ToTensor(),
@@ -93,40 +99,27 @@ def get_data_loaders(config, PATH):
         train_set = torchvision.datasets.MNIST(root=PATH, train=True, download=True, transform=transform)
         val_set = torchvision.datasets.MNIST(root=PATH, train=False, download=True, transform=transform)
         test_set = torchvision.datasets.MNIST(root=PATH, train=False, download=True, transform=transform)
-        num_classes = 10
-        config.data_variance = 1
 
     elif config.data_set == "CIFAR10":
-        transform=transforms.Compose([
-                transforms.ToTensor(),
-                transforms.Resize(config.image_size),
-                transforms.Normalize((0.5,0.5,0.5), (1.0,1.0,1.0))
-            ])
         train_set = torchvision.datasets.CIFAR10(root=PATH, train=True, download=True, transform=transform)
         val_set = torchvision.datasets.CIFAR10(root=PATH, train=False, download=True, transform=transform)
         test_set = torchvision.datasets.CIFAR10(root=PATH, train=False, download=True, transform=transform)
-        num_classes = 10
-        config.data_variance = np.var(train_set.data / 255.0)
 
     elif config.data_set == "FFHQ":
-        transform = transforms.Compose([
-                transforms.ToTensor(),
-                transforms.Resize(config.image_size),
-                transforms.Normalize((0.5,0.5,0.5), (1.0,1.0,1.0))
-            ])
-
         dataset = torchvision.datasets.ImageFolder(PATH, transform=transform)
         lengths = [int(len(dataset)*0.7), int(len(dataset)*0.1), int(len(dataset)*0.2)]
         train_set, val_set, test_set = random_split(dataset, lengths)
-
-        config.data_variance = 1#np.var(train_set.data / 255.0)
-        num_classes = 0
+    
+    elif config.data_set == "CELEBA":
+        train_set = torchvision.datasets.CelebA(root=PATH, split='train', download=True, transform=transform, target_type="identity")
+        val_set = torchvision.datasets.CelebA(root=PATH, split='valid', download=True, transform=transform, target_type="identity")
+        test_set = torchvision.datasets.CelebA(root=PATH, split='test', download=True, transform=transform, target_type="identity")
 
     train_loader = torch.utils.data.DataLoader(train_set, batch_size=config.batch_size, shuffle=True)
     val_loader = torch.utils.data.DataLoader(val_set, batch_size=config.batch_size, shuffle=False)
     test_loader = torch.utils.data.DataLoader(test_set, batch_size=config.batch_size, shuffle=False)
     
-    return train_loader, val_loader, test_loader, num_classes
+    return train_loader, val_loader, test_loader
 
         
 
